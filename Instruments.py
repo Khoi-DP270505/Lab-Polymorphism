2b205906285abd57dfa999275a29cd2f1848b2ae
Khoi-DP270505 <23011839@st.phenikaa-uni.edu.vn>
2024-09-23 13:51:56 +0700

    Add files via upload

---
+++ Instruments.py
@@ -0,0 +1,16 @@
+class Guitar:
+    def play(self):
+        print("playing the guitar")
+
+class Piano:
+    def play(self):
+        print("playing the piano")
+
+def play_instrument(instrument):
+    instrument.play()
+
+guitar = Guitar()
+play_instrument(guitar)  
+
+piano = Piano()
+play_instrument(piano)  
